c4b5bf6e42e386313fe341d410786f506439912a
ErciliaB <ercilliabucuane@gmail.com>
2026-05-16 15:12:57 +1000

    Added login dashboard and logout system

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h1>Welcome <?php echo $_SESSION['first_name']; ?>!</h1>
+
+<p>You are logged in.</p>
+
+<a href="logout.php">Logout</a>
+
+</body>
+</html>
