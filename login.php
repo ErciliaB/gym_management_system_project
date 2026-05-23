bb72c1a055e719de1cd332c3fcb6f06dd6006583
ErciliaB <ercilliabucuane@gmail.com>
2026-05-23 17:39:41 +1000

    Added PHP login logic on the file

--- login.php
+++ login.php
@@ -1,3 +1,39 @@
+<?php
+session_start();
+include 'db_connect.php';
+
+$message = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+
+        $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['first_name'] = $user['first_name'];
+
+            header("Location: dashboard.php");
+            exit();
+
+        } else {
+            $message = "Incorrect password!";
+        }
+
+    } else {
+        $message = "User not found!";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
